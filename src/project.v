1f8691963fccb1a235de39abfeaef1e9e9832a9f
曹亦佳 <caoyijia@nyu.edu>
2025-02-26 17:36:01 -0500

    Modified projcet.v

--- src/project.v
+++ src/project.v
@@ -1,44 +1,32 @@
-/*
- * Copyright (c) 2025 Yijia Cao
- * SPDX-License-Identifier: Apache-2.0
- */
-
-`default_nettype none
-
 module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+    input  wire [7:0] ui_in,    
+    output wire [7:0] uo_out,   
+    input  wire [7:0] uio_in,   
+    output wire [7:0] uio_out,  
+    output wire [7:0] uio_oe,   
+    input  wire       ena,      
+    input  wire       clk,      
+    input  wire       rst_n     
 );
 
-    wire [15:0] In;      // Concatenated input
-    assign In = {ui_in, uio_in};  // In[15:0] = {A[7:0], B[7:0]}
+    wire [15:0] In;      
+    assign In = {ui_in, uio_in};  
 
-    always @(*) begin
-        casez (In)  
-            16'b1???????????????: uo_out = 8'd15;
-            16'b01??????????????: uo_out = 8'd14;
-            16'b001?????????????: uo_out = 8'd13;
-            16'b0001????????????: uo_out = 8'd12;
-            16'b00001???????????: uo_out = 8'd11;
-            16'b000001??????????: uo_out = 8'd10;
-            16'b0000001?????????: uo_out = 8'd9;
-            16'b00000001????????: uo_out = 8'd8;
-            16'b000000001???????: uo_out = 8'd7;
-            16'b0000000001??????: uo_out = 8'd6;
-            16'b00000000001?????: uo_out = 8'd5;
-            16'b000000000001????: uo_out = 8'd4;
-            16'b0000000000001???: uo_out = 8'd3;
-            16'b00000000000001??: uo_out = 8'd2;
-            16'b000000000000001?: uo_out = 8'd1;
-            16'b0000000000000001: uo_out = 8'd0;
-            16'b0000000000000000: uo_out = 8'b11110000; // Special case
-            default: uo_out = 8'b00000000;
-        endcase
-    end
+    assign uo_out = (In[15] ? 8'd15 :
+                     In[14] ? 8'd14 :
+                     In[13] ? 8'd13 :
+                     In[12] ? 8'd12 :
+                     In[11] ? 8'd11 :
+                     In[10] ? 8'd10 :
+                     In[9]  ? 8'd9  :
+                     In[8]  ? 8'd8  :
+                     In[7]  ? 8'd7  :
+                     In[6]  ? 8'd6  :
+                     In[5]  ? 8'd5  :
+                     In[4]  ? 8'd4  :
+                     In[3]  ? 8'd3  :
+                     In[2]  ? 8'd2  :
+                     In[1]  ? 8'd1  :
+                     In[0]  ? 8'd0  : 8'b11110000); // Special case
+
 endmodule
